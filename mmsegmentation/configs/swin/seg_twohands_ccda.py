4dbb7e28193392ef328b0243a57e298eb706e0b0
yufu <yufu@github.com>
2024-02-20 12:50:22 +0000

    update

--- mmsegmentation/configs/swin/seg_twohands_ccda.py
+++ mmsegmentation/configs/swin/seg_twohands_ccda.py
@@ -13,8 +13,8 @@ model = dict(
         depths=[2, 2, 18, 2],
         num_heads=[4, 8, 16, 32],
         window_size=12),
-    decode_head=dict(in_channels=[128, 256, 512, 1024], num_classes=3),
-    auxiliary_head=dict(in_channels=512, num_classes=3))
+    decode_head=dict(in_channels=[128, 256, 512, 1024], num_classes=2),
+    auxiliary_head=dict(in_channels=512, num_classes=2))
 
 train_pipeline = [dict(type='LoadAnnotations', reduce_zero_label=False)]
 
--- mmsegmentation/test_with_miou.sh
+++ mmsegmentation/test_with_miou.sh
@@ -4,4 +4,4 @@ work_dirs/seg_twohands_lama_ccda_3cls/seg_twohands_ccda.py \
 work_dirs/seg_twohands_lama_ccda_3cls/best_mIoU_iter_86000.pth \
 --work-dir work_dirs/seg_twohands_lama_ccda_3cls/ \
 --eval mIoU \
---show-dir  work_dirs/seg_twohands_lama_ccda_3cls/best_mIoU_iter_86000_test
+--show-dir  work_dirs/seg_twohands_lama_ccda_3cls/best_mIoU_iter_86000_test_out
