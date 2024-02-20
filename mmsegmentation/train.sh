4b3731035e4ec0f348fea88ea8500c59f8c0bc61
yufu <yufu@github.com>
2024-02-20 13:33:10 +0000

    update

--- mmsegmentation/train.sh
+++ mmsegmentation/train.sh
@@ -28,7 +28,7 @@
 
 # rgb -> twohands
 #python tools/train.py ./configs/swin/seg_twohands_ccda.py  --work-dir ./work_dirs/seg_twohands_ccda_3cls
-./tools/dist_train.sh ./configs/swin/seg_twohands_ccda.py 2 ./work_dirs/seg_twohands_lama_ccda_3clsPaste_2labelPred
+./tools/dist_train.sh ./configs/swin/seg_twohands_ccda.py 2 ./work_dirs/seg_twohands_lama_ccda_3clsPaste_2labelPred_3datasets
 # ./tools/dist_train.sh ./configs/swin/seg_twohands_ccda.py 4
 
 
